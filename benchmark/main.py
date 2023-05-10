53693a0b33e311de3091630868249c698daeff33
Sergei Telnov <sergeitelnow@gmail.com>
2023-05-10 06:59:41 +0300

    handle any error during running benchmark

--- benchmark/main.py
+++ benchmark/main.py
@@ -5,23 +5,23 @@ from benchmark.utils import Print, BenchError
 def local(debug=True):
     """ Run benchmarks on localhost """
     bench_params = {
-        'faults': 5,
-        'nodes': 16,
+        'faults': 1,
+        'nodes': 5,
         'transactions_number': 200_000,
         'transactions_per_consensus': 10,
-        'duration': 5 * 60
+        'duration': 15
     }
 
     nodes = bench_params['nodes']
     faults = bench_params['faults']
     duration = bench_params['duration']
 
-    for i in range(5):
+    for i in range(120):
         print(f'Run call number {i + 1}')
         try:
             ret = LocalBench(bench_params).run(debug)
             ret.print(f'results/.results-n-{nodes}-f-{faults}-duration-{duration}.txt')
-        except BenchError as e:
+        except Exception as e:
             Print.error(e)
 
 
